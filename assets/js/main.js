9d174b0ce2f1b3ddc9bcfd47b9aee85f637d787a
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-17 12:13:00 +0200

    Fetch and store posts from API, logging the results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("Prova")
+const posts = [];
+
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => {
+            posts.push(post);
+        });
+    })
+
+console.log(posts)
