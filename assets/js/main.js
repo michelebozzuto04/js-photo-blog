3c6e7f4a04fa3808994d2005bd04fdef9a30415d
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-17 13:16:10 +0200

    Refactor data fetching and rendering logic for posts, updating HTML structure for dynamic content display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
+const rowEl = document.querySelector('.row')
 const posts = [];
 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
     .then(response => response.json())
     .then(data => {
-        data.forEach(post => {
-            posts.push(post);
+        renderString = ``;
+        data.forEach(element => {
+            console.log(element)
+            renderString += `<div class="col-lg-4"><div class="card d-flex">
+                        <div class="card-pin">
+                            <img src="./assets/img/pin.svg" alt="">
+                        </div>
+                        <div class="card-image">
+                            <img id="card-image" src='${element.url}' alt="">
+                        </div>
+                        <div class="card-description">
+                            <span id="card-description">${element.title}</span>
+                        </div>
+                    </div></div>`
+            rowEl.innerHTML = renderString;
         });
     })
-
-console.log(posts)
+    .catch(error => console.error(error))
