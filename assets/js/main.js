1e884211313933a10d8ad1a7fbdaf43464b9def2
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-18 10:43:38 +0200

    Add modal functionality and enhance card display in main.js and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 const rowEl = document.querySelector('.row')
+const modalEl = document.getElementById("modal");
 const posts = [];
+
+function openModal() {
+    modalEl.classList.remove('d-none');
+    modalEl.classList.add('d-flex');
+}
+
+function closeModal() {
+    modalEl.classList.remove('d-flex');
+    modalEl.classList.add('d-none');
+}
 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
     .then(response => response.json())
@@ -7,18 +18,30 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
         renderString = ``;
         data.forEach(element => {
             console.log(element)
-            renderString += `<div class="col-lg-4 col-md-6 col-sm-12"><div class="card d-flex">
-                        <div class="card-pin">
-                            <img src="./assets/img/pin.svg" alt="">
-                        </div>
-                        <div class="card-image">
-                            <img id="card-image" src='${element.url}' alt="">
-                        </div>
-                        <div class="card-description">
-                            <span id="card-description">${element.title}</span>
-                        </div>
-                    </div></div>`
-            rowEl.innerHTML = renderString;
+            const colEl = document.createElement('div');
+            colEl.classList.add('col-lg-4', 'col-md-6', 'col-sm-12');
+            const cardEl = document.createElement('div');
+            cardEl.classList.add('card', 'd-flex');
+            const cardPinEl = document.createElement('div');
+            cardPinEl.classList.add('card-pin');
+            const cardImageEl = document.createElement('div');
+            cardImageEl.classList.add('card-image');
+            const cardDescriptionEl = document.createElement('div');
+            cardDescriptionEl.classList.add('card-description');
+            cardPinEl.innerHTML = '<img src="./assets/img/pin.svg" alt=""></img>';
+            cardImageEl.innerHTML = `<img id="card-image" src='${element.url}' alt="">`;
+            cardDescriptionEl.innerHTML = `<span id="card-description">${element.title}</span>`;
+            cardEl.append(cardPinEl, cardImageEl, cardDescriptionEl);
+            colEl.appendChild(cardEl);
+            rowEl.appendChild(colEl);
+
+            cardEl.addEventListener('click', () => {
+                openModal()
+                modalEl.addEventListener('click', () => {
+                    closeModal()
+                })
+            })
         });
     })
     .catch(error => console.error(error))
+
